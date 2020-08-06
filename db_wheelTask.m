9ad6e8934034bfd77d0ce8b32073ea49a35cba44
sylviaschroeder <sylvia.schroeder@ucl.ac.uk>
2020-08-06 11:45:16 +0100

    Added db_wheelTask

---
+++ db_wheelTask.m
@@ -0,0 +1,19 @@
+k = 0;
+% (1)
+k=k+1;
+db(k).subject = 'SS041';
+db(k).date = '2015-08-11';
+db(k).exp = 1;
+db(k).microID = 'b2';
+% (2)
+k=k+1;
+db(k).subject = 'SS047';
+db(k).date = '2015-11-10';
+db(k).exp = 4:6;
+db(k).microID = 'b';
+% (3)
+k=k+1;
+db(k).subject = 'SS047';
+db(k).date = '2015-11-11';
+db(k).exp = 2;
+db(k).microID = 'b';
